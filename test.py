0b363686b1e8c1344aa62be0dbf0f33adf5a6a94
Vytenis Bivainis <monrealis@gmail.com>
2015-06-02 21:04:23 +0300

    Print prime numbers with yielding function.

--- test.py
+++ test.py
@@ -1,2 +1,25 @@
 __author__ = 'vytenis'
 print("Hello")
+
+
+def isPrime(n):
+    if n < 2:
+        return
+    for i in range(2, n - 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def getPrimes():
+    for i in range(1, 10000):
+        if isPrime(i):
+            yield i
+
+
+n = 0
+for i in getPrimes():
+    print(i, end=' ')
+    n += 1
+    if n == 10:
+        break
