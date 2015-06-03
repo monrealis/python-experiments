1d3121ed81f472b721d74f7c64a87d6eb1eeb0d5
Vytenis Bivainis <monrealis@gmail.com>
2015-06-03 21:31:07 +0300

    Change order of functions.

--- primes.py
+++ primes.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+def printFirstTenPrimes():
+    n = 0
+    for i in getPrimes():
+        print(i, end=' ')
+        n += 1
+        if n == 10:
+            print()
+            break
+
+def getPrimes():
+    for i in range(1, 10000):
+        if isPrime(i):
+            yield i
+
 def isPrime(n):
     if n < 2:
         return
@@ -8,17 +22,4 @@ def isPrime(n):
             return False
     return True
 
-
-def getPrimes():
-    for i in range(1, 10000):
-        if isPrime(i):
-            yield i
-
-
-n = 0
-for i in getPrimes():
-    print(i, end=' ')
-    n += 1
-    if n == 10:
-        print()
-        break
+if __name__ == 'main': printFirstTenPrimes()
