dce0f3045b1110db35055e906a6443fc2fb79d6f
Vytenis Bivainis <monrealis@gmail.com>
2015-06-06 13:01:50 +0300

    Create class PrimesPrinter.

--- primes.py
+++ primes.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python3
 
-def print_first_ten_primes():
-    n = 0
-    for i in get_primes():
-        print(i, end=' ')
-        n += 1
-        if n == 10:
-            print()
-            break
+class PrimesPrinter:
+    def print_first_ten_primes(self):
+        n = 0
+        for i in self.get_primes():
+            print(i, end=' ')
+            n += 1
+            if n == 10:
+                print()
+                break
 
-def get_primes():
-    for i in range(1, 10000):
-        if is_prime(i):
-            yield i
+    def get_primes(self):
+        for i in range(1, 10000):
+            if self.is_prime(i):
+                yield i
 
-def is_prime(n):
-    if n < 2:
-        return
-    for i in range(2, n - 1):
-        if n % i == 0:
-            return False
-    return True
+    def is_prime(self, n):
+        if n < 2:
+            return
+        for i in range(2, n - 1):
+            if n % i == 0:
+                return False
+        return True
 
 
-if __name__ == '__main__': print_first_ten_primes()
+if __name__ == '__main__': PrimesPrinter().print_first_ten_primes()
