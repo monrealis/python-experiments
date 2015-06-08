bd5c7150b7dc168ebf3195553aed29d24acde323
Vytenis Bivainis <monrealis@gmail.com>
2015-06-08 21:45:40 +0300

    Add first unit test.

--- xml_visualizer.py
+++ xml_visualizer.py
@@ -5,5 +5,13 @@ class XmlVisualizerTest(unittest.TestCase):
     def setUp(self):
         super(XmlVisualizerTest, self).setUp()
 
-    def test(self):
-        ET.fromstring('<root />')
+    def test_visualizes_root_element(self):
+        self.assertEqual('root', XmlVisualizer('<root />').visualize())
+
+
+class XmlVisualizer:
+    def __init__(self, xml):
+        self.tree = ET.fromstring(xml)
+
+    def visualize(self):
+        return str(self.tree.tag)
