c53b99a09dfb5c34741b5cd9ca6cd031e9ff82e3
Vytenis Bivainis <monrealis@gmail.com>
2015-06-09 21:31:28 +0300

    Add skipped tests.

--- xml_visualizer.py
+++ xml_visualizer.py
@@ -5,8 +5,20 @@ class XmlVisualizerTest(unittest.TestCase):
     def setUp(self):
         super(XmlVisualizerTest, self).setUp()
 
+
     def test_visualizes_root_element(self):
-        self.assertEqual('root', XmlVisualizer('<root />').visualize())
+        self.assertEqual('root', self.visualize('<root />'))
+
+    @unittest.SkipTest
+    def test_visualizes_root_element_with_ns(self):
+        self.assertEqual('root', self.visualize('<root xmlns="urn:test:test" />'))
+
+    @unittest.SkipTest
+    def test_visualizes_root_element_with_prefix(self):
+        self.assertEqual('root', self.visualize('<r:root xmlns:r="urn:test:test" />'))
+
+    def visualize(self, xml):
+        return XmlVisualizer(xml).visualize()
 
 
 class XmlVisualizer:
@@ -15,3 +27,9 @@ class XmlVisualizer:
 
     def visualize(self):
         return str(self.tree.tag)
+
+
+    def dump(self, obj):
+        for attr in dir(obj):
+            print("obj.%s = %s" % (attr, getattr(obj, attr)))
+
