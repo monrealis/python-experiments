71e3a57253ba0c75089aa7f61da8e3f5dcd0ef4f
Vytenis Bivainis <monrealis@gmail.com>
2015-06-10 21:23:45 +0300

    Use QName class.

--- xml_visualizer.py
+++ xml_visualizer.py
@@ -1,19 +1,20 @@
 import unittest
+
 import xml.etree.ElementTree as ET
+
+import lxml.etree as et
+
 
 class XmlVisualizerTest(unittest.TestCase):
     def setUp(self):
         super(XmlVisualizerTest, self).setUp()
 
-
     def test_visualizes_root_element(self):
         self.assertEqual('root', self.visualize('<root />'))
 
-    @unittest.SkipTest
     def test_visualizes_root_element_with_ns(self):
         self.assertEqual('root', self.visualize('<root xmlns="urn:test:test" />'))
 
-    @unittest.SkipTest
     def test_visualizes_root_element_with_prefix(self):
         self.assertEqual('root', self.visualize('<r:root xmlns:r="urn:test:test" />'))
 
@@ -26,10 +27,9 @@ class XmlVisualizer:
         self.tree = ET.fromstring(xml)
 
     def visualize(self):
-        return str(self.tree.tag)
-
+        q = et.QName(self.tree.tag)
+        return str(q.localname)
 
     def dump(self, obj):
         for attr in dir(obj):
             print("obj.%s = %s" % (attr, getattr(obj, attr)))
-
